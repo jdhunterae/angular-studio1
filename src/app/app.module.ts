9128b02f16bc6df1094000d4bfe0b6d7bcdaa4c8
Andrew Pomerleau <arpomerleau@gmail.com>
2022-06-27 20:06:50 -0400

    Angular gen equipment component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CrewComponent
+    CrewComponent,
+    EquipmentComponent
   ],
   imports: [
     BrowserModule
